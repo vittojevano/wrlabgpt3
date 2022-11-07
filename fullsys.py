6cebd2bc57bfd696c7ce8b5600e731fbb638e5ad
vittojevano <88228805+vittojevano@users.noreply.github.com>
2022-11-07 17:46:20 +0900

    Update fullsys.py

--- fullsys.py
+++ fullsys.py
@@ -31,10 +31,10 @@ class SysState(object):
         # GPT3からの応答を聞き、文章を生成するためのプール思考モジュール
         get_respond_state, respond_message = self._gptsys.get_respond()
 
-        print("Listening now")
 
         if get_respond_state == GetRespondState.HearingTimeOut:
             # 人間から連絡がない、最後の会話を記録から削除する。
+            print("Listening now")
             self._gptsys.remove_last_conversation()
         elif get_respond_state == GetRespondState.GPT3TimeOut:
             # GPT3の問題が発生した場合、最後の会話を削除します。
